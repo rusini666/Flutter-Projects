f9d9c579becc4e6cc1a65c798ebf5fe2f70ca02a
Rusini Thara <72293107+rusini666@users.noreply.github.com>
2022-02-24 19:51:05 +0530

    Addedunit converter project

---
+++ unit_converter/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Measures Converter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Measures Converter'),
+        ),
+        body: Center(
+          child: Text('Measures Converter'),
+        ),),
+    );}
+}
